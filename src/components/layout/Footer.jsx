0fe05a17256de9af48a523e800d1286b28d26414
philjang <pjang7390@gmail.com>
2022-03-25 12:22:25 -0700

    change className

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer>
             <p>© Saucy Syntax Inc. {new Date().getFullYear()}</p>
             <div>
-            <a target="_blank" rel="noreferrer" href="https://github.com/evankski/day-in-life-client"><i class="fab fa-github"></i></a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/evankski/day-in-life-client"><i className="fab fa-github"></i></a>
             </div>
         </footer>
     );
