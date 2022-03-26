57c1724fe14f22a08e19888d815ed112f1c2242a
philjang <pjang7390@gmail.com>
2022-03-26 16:13:09 -0700

    move bio structure into partial component

--- src/components/partials/Bio.jsx
+++ src/components/partials/Bio.jsx
@@ -1,5 +1,10 @@
-export default function Bio() {
+export default function Bio({ member, idx }) {
     return (
-        <h4>Bio Component</h4>
+        <div>
+            <img src={`${member.image_url}`} alt={`Picture of ${member.name}`} width={'20%'} style={{borderRadius: 400/ 2}}/>
+            <h1>{member.name}</h1>
+            <p>{member.bio}</p>
+            <a href={`mailto:${member.email}`}> <img src="https://www.iconpacks.net/icons/2/free-send-mail-icon-2574-thumb.png" width={'10%'} alt={`Email ${member.name} button`} /> </a>
+        </div>
     )
 }
