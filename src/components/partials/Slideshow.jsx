b5118e41d50e483c48de0ab73b79c061f2ad052d
philjang <pjang7390@gmail.com>
2022-03-26 23:11:38 -0700

    add basic function to Feed.jsx

--- src/components/partials/Slideshow.jsx
+++ src/components/partials/Slideshow.jsx
@@ -1,5 +1,13 @@
-export default function Slideshow() {
+import { Link } from 'react-router-dom'
+
+export default function Slideshow({ user }) {
     return (
-        <h4>Slideshow Component</h4>
+        <>
+            <Link to={`/profiles/${user._id}`}>
+                <h3>{user.name}</h3>
+                <h4>{user.photos.length}</h4>
+                <img src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[0].public_id}`} alt="user photo" /> 
+            </Link>
+        </>
     )
 }
