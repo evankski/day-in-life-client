af1172fa47bea3367d4f9a005134d226b0926d08
evankski <evankarwowski@gmail.com>
2022-03-28 10:50:31 -0700

    adds random photo for each user on refresh

--- src/components/partials/Slideshow.jsx
+++ src/components/partials/Slideshow.jsx
@@ -1,13 +1,42 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Slideshow({ user }) {
-    return (
-        <>
-            <Link to={`/profiles/${user._id}`}>
-                <h3>{user.name}</h3>
-                <h4>{user.photos.length}</h4>
-                <img src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[0].public_id}`} alt="user photo" /> 
-            </Link>
-        </>
-    )
+  const [imageIndex, setImageIndex] = useState(0);
+
+  // useEffect(() => {
+  //         setInterval(() => {
+  //         if(imageIndex > user.photos.length) {
+  //             setImageIndex(0)
+  //         } else {
+  //             setImageIndex(imageIndex + 1)
+  //         }
+  //     }, 3000)
+  // })
+
+  // const interval = setInterval(() => {
+  //         imageIndex+1
+  //         return imageIndex
+  //         }, 3000)
+  // // interval()
+
+  console.log(imageIndex);
+
+  return (
+    <>
+      <Link to={`/profiles/${user._id}`}>
+        <h3>{user.name}</h3>
+        <h4>{user.photos.length}</h4>
+          <img
+            src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[Math.floor(Math.random()*user.photos.length)].public_id}`}
+            alt="user photo"
+          />
+          {/* <img
+            src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[0].public_id}`}
+            alt="user photo"
+          /> */}
+        )
+      </Link>
+    </>
+  );
 }
