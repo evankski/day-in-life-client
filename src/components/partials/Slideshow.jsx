8e47d3b90df2452c71c92c2c890e23265cbbe0fc
evankski <evankarwowski@gmail.com>
2022-03-28 14:02:58 -0700

    funcitoning slideshow

--- src/components/partials/Slideshow.jsx
+++ src/components/partials/Slideshow.jsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 export default function Slideshow({ user }) {
   const [imageIndex, setImageIndex] = useState(0);
 
+  // Randomizes the index depending on the amount of photos uploaded by a user
   useEffect(() => {
           setInterval(() => {
           setImageIndex(Math.floor(Math.random()*user.photos.length))
       }, 3000)
   }, [])
+
 
   return (
     <>
@@ -23,6 +25,8 @@ export default function Slideshow({ user }) {
             src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[imageIndex].public_id}`}
             alt="user photo"
             width={'20%'}
+            id="userImg"
+            // className="animate__animated animate__fadeIn animate__infinite animate__slower"
           />
       </Link>
     </>
