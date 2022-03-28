091de6c33473209d7a37668f38f6452eb98f970e
evankski <evankarwowski@gmail.com>
2022-03-28 11:00:54 -0700

    adds slideshow component

--- src/components/partials/Slideshow.jsx
+++ src/components/partials/Slideshow.jsx
@@ -4,38 +4,26 @@ import { Link } from "react-router-dom";
 export default function Slideshow({ user }) {
   const [imageIndex, setImageIndex] = useState(0);
 
-  // useEffect(() => {
-  //         setInterval(() => {
-  //         if(imageIndex > user.photos.length) {
-  //             setImageIndex(0)
-  //         } else {
-  //             setImageIndex(imageIndex + 1)
-  //         }
-  //     }, 3000)
-  // })
-
-  // const interval = setInterval(() => {
-  //         imageIndex+1
-  //         return imageIndex
-  //         }, 3000)
-  // // interval()
-
-  console.log(imageIndex);
+  useEffect(() => {
+          setInterval(() => {
+          setImageIndex(Math.floor(Math.random()*user.photos.length))
+      }, 3000)
+  }, [])
 
   return (
     <>
       <Link to={`/profiles/${user._id}`}>
         <h3>{user.name}</h3>
         <h4>{user.photos.length}</h4>
-          <img
+          {/* <img
             src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[Math.floor(Math.random()*user.photos.length)].public_id}`}
             alt="user photo"
-          />
-          {/* <img
-            src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[0].public_id}`}
-            alt="user photo"
           /> */}
-        )
+          <img
+            src={`https://res.cloudinary.com/dhs1wrqhp/image/upload/${user.photos[imageIndex].public_id}`}
+            alt="user photo"
+            width={'20%'}
+          />
       </Link>
     </>
   );
