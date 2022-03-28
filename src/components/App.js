4527f95594fa0623ae3fb2cdf1e7d58bac9f8081
danieldenton <danielmdenton@gmail.com>
2022-03-28 14:13:34 -0700

    prifile complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,82 +1,106 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useState, useEffect } from 'react'
-import jwt_decode from 'jwt-decode'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
-import '../App.css';
+import "../App.css";
 
-import Layout from './layout/Layout';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import About from './pages/About';
-import Feed from './pages/Feed';
-import Profile from './pages/Profile';
-import Picture from './pages/Picture';
-import axios from 'axios';
+import Layout from "./layout/Layout";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import About from "./pages/About";
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
+import Picture from "./pages/Picture";
+import axios from "axios";
 
 function App() {
-
   // STATE
-    // state with the user data when the user is logged in
-    const [currentUser, setCurrentUser] = useState(null)
+  // state with the user data when the user is logged in
+  const [currentUser, setCurrentUser] = useState(null);
 
-    // state with all users
-    const [users, setUsers] = useState([])
+  // state with all users
+  const [users, setUsers] = useState([]);
 
   // USE
-    // useEffect that handles localstorage if the user navigates away from the page/refreshes
-    useEffect(() => {
-      const token = localStorage.getItem('jwt')
-      // if a token is found, log the user in; otherwise, make sure they are logged out
-      if (token) {
-        setCurrentUser(jwt_decode(token))
-      } else {
-        setCurrentUser(null)
-      }
-
-      // // setting state with all users from db
-      // axios.get(process.env.REACT_APP_SERVER_URL+'/api-v1/users')
-      //   .then(res => {
-      //     // console.log(res.data)
-      //     setUsers(res.data)
-      //   })
-      //   .catch(console.log)
-    }, [])
-    // logout handler function that deletes a token from localstorage
-    const handleLogout = () => {
-      // remove the token from local storage
-      if (localStorage.getItem('jwt')) localStorage.removeItem('jwt')
-    
-      // set the user state to be null
-      setCurrentUser(null)
+  // useEffect that handles localstorage if the user navigates away from the page/refreshes
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    // if a token is found, log the user in; otherwise, make sure they are logged out
+    if (token) {
+      setCurrentUser(jwt_decode(token));
+    } else {
+      setCurrentUser(null);
     }
 
-    return (
-      <div className="App">
-        <Router>
-          <Layout handleLogout={handleLogout} currentUser={currentUser} >
-                <Routes>
-                    
-                    <Route path="/" element={<Landing />} />
+    // // setting state with all users from db
+    // axios.get(process.env.REACT_APP_SERVER_URL+'/api-v1/users')
+    //   .then(res => {
+    //     // console.log(res.data)
+    //     setUsers(res.data)
+    //   })
+    //   .catch(console.log)
+  }, []);
+  // logout handler function that deletes a token from localstorage
+  const handleLogout = () => {
+    // remove the token from local storage
+    if (localStorage.getItem("jwt")) localStorage.removeItem("jwt");
 
-                    <Route path="/login" element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser} />} />
+    // set the user state to be null
+    setCurrentUser(null);
+  };
 
-                    <Route path="/register" element={<Register currentUser={currentUser} setCurrentUser={setCurrentUser} />} />
+  return (
+    <div className="App">
+      <Router>
+        <Layout handleLogout={handleLogout} currentUser={currentUser}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
 
-                    <Route path='/about' element={<About />} />
+            <Route
+              path="/login"
+              element={
+                <Login
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
 
-                    <Route path='/feed' element={<Feed />} />
-                    {/* <Route path='/feed' element={currentUser ? <Feed currentUser={currentUser} /> : <Navigate to='/login' />} /> */}
+            <Route
+              path="/register"
+              element={
+                <Register
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
 
-                    <Route path='/profiles/:id' element={<Profile />} />
+            <Route path="/about" element={<About />} />
 
-                    <Route path='/pictures/:id' element={<Picture setUsers={setUsers} />} />
+            <Route path="/feed" element={<Feed />} />
+            {/* <Route path='/feed' element={currentUser ? <Feed currentUser={currentUser} /> : <Navigate to='/login' />} /> */}
 
-                </Routes>
-          </Layout>
-        </Router>
-      </div>
-    );
+            <Route
+              path="/profiles/:id"
+              element={<Profile currentUser={currentUser} />}
+            />
+
+            <Route
+              path="/pictures/:id"
+              element={<Picture setUsers={setUsers} />}
+            />
+          </Routes>
+        </Layout>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -3,13 +3,17 @@ import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function Profile() {
+export default function Profile({ currentUser, setCurrentUser }) {
   const { id } = useParams();
   const [photos, setPhotos] = useState([]);
-  const [userName, setUserName] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [showEdit, setShowEdit] = useState(false);
+  const [ownerId, setOwnerId] = useState("");
+
   useEffect(() => {
     (async () => {
       try {
+        console.log("hello");
         const token = localStorage.getItem("jwt");
         // console.log(token);
         const options = {
@@ -23,27 +27,61 @@ export default function Profile() {
         );
         console.log(response.data);
         setPhotos(response.data.photos);
-        setUserName(response.data.name);
+        setOwnerName(response.data.name);
+        setOwnerId(response.data._id);
       } catch (err) {
         console.log(err);
       }
     })();
-  }, []);
+  }, [showEdit]);
+
+  const onButtonClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleDelete = async (photoId) => {
+    try {
+      const token = localStorage.getItem("jwt");
+      const options = {
+        headers: {
+          Authorization: token,
+        },
+      };
+      await axios.delete(
+        `${process.env.REACT_APP_SERVER_URL}/api-v1/pictures/${photoId}`,
+        options
+      );
+      setShowEdit(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const userPhotos = photos.map((photo) => {
     const cloudImage = `https://res.cloudinary.com/dhs1wrqhp/image/upload/v1593119998/${photo.public_id}.png`;
 
     return (
       <div key={photo.public_id}>
-        <Link to={`pictures/${photo._id}`}>
+        <Link to={`/pictures/${photo._id}`}>
           <img src={cloudImage} key={photo.public_id} />
         </Link>
+        {showEdit && (
+          <button onClick={() => handleDelete(photo._id)}>delete</button>
+        )}
       </div>
     );
   });
   return (
     <div>
-      <h1>{userName}</h1>
+      <h1>{ownerName}</h1>
+      {currentUser ? (
+        ownerId === currentUser.id ? (
+          <button onClick={() => onButtonClick()}>
+            {showEdit ? "done editing" : "edit"}
+          </button>
+        ) : null
+      ) : null}
+
       {userPhotos}
     </div>
   );
