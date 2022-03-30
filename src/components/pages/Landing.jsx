7e978ae3fe07ca929d996c7d6898e28b18d74fcd
evankski <evankarwowski@gmail.com>
2022-03-30 11:06:00 -0700

    landing page styling

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -5,19 +5,26 @@ import landing from "../../landing/landing"
 export default function Landing ({ currentUser }) {
     if (currentUser) return <Navigate to='/feed' />
     return (
-        <>
-            <div className="app-explanation-one">
-                <img src={landing[0].image} alt="" width={`10%`} className="image0"/>
-                <h3 className="landing-text">{landing[0].content}</h3>
+        <div className="landing-page">
+
+            <div className="app-explanation-one animate__animated animate__fadeIn">
+                {/* <img src={landing[0].image} alt="" width={`10%`} className="image0"/> */}
+                <h1 className="landing-text-one">{landing[0].header}</h1>
+                <p className="landing-p-one">{landing[0].content}</p>
+                <a href={landing[0].github} className="github-button">View on Github</a>
             </div>
-            <div className="app-explanation-two">
-                <img src={landing[1].image} alt="" width={`10%`} className="image1"/>
+            <div className="app-explanation-two animate__animated animate__fadeIn">
+            
+                <h3 className="landing-text">{landing[1].content}</h3>
+            
+                <h3 className="landing-text">{landing[1].content}</h3>
+            
                 <h3 className="landing-text">{landing[1].content}</h3>
             </div>
-            <div className="app-explanation">
+            <div className="app-explanation animate__animated animate__fadeIn">
                 <img src={landing[2].image} alt="" width={`10%`} className="image0"/>
                 <h3 className="landing-text">{landing[2].content}</h3>
             </div>
-        </>
+        </div>
     )
 }
--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,7 +1,9 @@
 const landing = [
     {
-        content: 'this is explaining how it is going to be more of a personal social media Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        image: 'https://www.pngitem.com/pimgs/m/576-5768680_avatar-png-icon-person-icon-png-free-transparent.png'
+        header: 'A Day In The Life',
+        content: 'A day in the life is a MERN stack application that utelizes the Cloudinary API. We aimed to make a social media that focuses more on the needs of the user and less about pumping new fancy features to distract the user. Our goal is to make casual photo sharing more engaging, similar to the early days of social media.',
+        image: 'https://www.pngitem.com/pimgs/m/576-5768680_avatar-png-icon-person-icon-png-free-transparent.png',
+        github: 'https://github.com/evankski/day-in-life-client'
     },
     {
         content: 'This is showing an example of what it looks like to click on someones profile Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
