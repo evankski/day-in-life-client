bc800e21f95fe6038038061ad1e2ff021c082bab
evankski <evankarwowski@gmail.com>
2022-03-31 10:19:03 -0700

    adds linked in icons

--- src/components/partials/Bio.jsx
+++ src/components/partials/Bio.jsx
@@ -4,7 +4,11 @@ export default function Bio({ member }) {
             <img className='center' src={`${member.image_url}`} alt={`Picture of ${member.name}`} width={'60%'} style={{borderRadius: 400/ 2}}/>
             <h1 className="member-name">{member.name}</h1>
             <p>{member.bio}</p>
-            <a href={`mailto:${member.email}`}> <img src="https://i.imgur.com/iZDfazp.png" width={'20%'} alt={`Email ${member.name} button`} /> </a>
+
+            <div className="socials">
+                <a href={`mailto:${member.email}`}> <img src="https://i.imgur.com/iZDfazp.png" width={'20%'} alt={`Email ${member.name} button`} /> </a>
+                <a href={member.linkedin}> <i className='fab fa-linkedin social-li'></i> </a>
+            </div>
         </div>
     )
 }
