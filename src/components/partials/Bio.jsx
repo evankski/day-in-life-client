552811bed9bd57bcc440a85bcfbc5fe7e266731a
evankski <evankarwowski@gmail.com>
2022-03-31 09:41:03 -0700

    inverts email color

--- src/components/partials/Bio.jsx
+++ src/components/partials/Bio.jsx
@@ -4,7 +4,7 @@ export default function Bio({ member }) {
             <img className='center' src={`${member.image_url}`} alt={`Picture of ${member.name}`} width={'60%'} style={{borderRadius: 400/ 2}}/>
             <h1 className="member-name">{member.name}</h1>
             <p>{member.bio}</p>
-            <a href={`mailto:${member.email}`}> <img src="https://www.iconpacks.net/icons/2/free-send-mail-icon-2574-thumb.png" width={'10%'} alt={`Email ${member.name} button`} /> </a>
+            <a href={`mailto:${member.email}`}> <img src="https://i.imgur.com/iZDfazp.png" width={'20%'} alt={`Email ${member.name} button`} /> </a>
         </div>
     )
 }
