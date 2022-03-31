14152cc1001ae808b4911a7991b334ebb9a654da
evankski <evankarwowski@gmail.com>
2022-03-31 10:31:35 -0700

    finishes about us page

--- src/components/partials/Bio.jsx
+++ src/components/partials/Bio.jsx
@@ -1,13 +1,14 @@
 export default function Bio({ member }) {
     return (
-        <div className='column bg-light'>
+        <div className='column bg-light bios'>
             <img className='center' src={`${member.image_url}`} alt={`Picture of ${member.name}`} width={'60%'} style={{borderRadius: 400/ 2}}/>
             <h1 className="member-name">{member.name}</h1>
             <p>{member.bio}</p>
 
             <div className="socials">
-                <a href={`mailto:${member.email}`}> <img src="https://i.imgur.com/iZDfazp.png" width={'20%'} alt={`Email ${member.name} button`} /> </a>
-                <a href={member.linkedin}> <i className='fab fa-linkedin social-li'></i> </a>
+                <a href={`mailto:${member.email}`}> <i className='fas fa-envelope'></i> </a>
+                <a href={member.linkedin}> <i className='fab fa-linkedin'></i> </a>
+                <a href={member.github}> <i className='fab fa-github'></i> </a>
             </div>
         </div>
     )
--- src/members.js
+++ src/members.js
@@ -4,21 +4,24 @@ const members = [
         image_url: 'https://media-exp1.licdn.com/dms/image/C5603AQE1c7YIkpYRgA/profile-displayphoto-shrink_200_200/0/1640053888076?e=1653523200&v=beta&t=dCwnO96lar0slQoDyJd10Ft2yssIjfe2Dq-IoYBNTNA',
         email: 'evan@thewowski.com',
         bio: 'Evan is a Software Engineer and a Real Estate Agent who thrives in high pressure situations. Having helped build a Real Estate company from the ground up, he enjoys teaching himself new and complex ideas. When working with a team, He is never afraid to tackle any challenge thrown his way.',
-        linkedin: 'https://www.linkedin.com/in/evan-karwowski/'
+        linkedin: 'https://www.linkedin.com/in/evan-karwowski/',
+        github: 'https://github.com/evankski'
     },
     {
         name: 'Phil Jang',
         image_url: 'https://media-exp1.licdn.com/dms/image/C4E03AQHmnOoqjmkiCA/profile-displayphoto-shrink_200_200/0/1644913025222?e=1653523200&v=beta&t=atJIcFjdNRzFS_Q8uD20z8c0gMUqFpClivxJLN0SNCk',
         email: 'philjang.pr@gmail.com',
         bio: 'Phil is a software developer with a cerebral mindset and natural curiosity. He values user-focused solutions that supplement the human experience and quality of life. With experience in the healthcare and service industries, he believes in utilizing the balance between diversity of thought and methodical analytics to pursue efficient, sustainable progress.',
-        linkedin: 'https://www.linkedin.com/in/phil-jang/'
+        linkedin: 'https://www.linkedin.com/in/phil-jang/',
+        github: 'https://github.com/philjang'
     },
     {
         name: 'Daniel Denton',
         image_url: 'https://media-exp1.licdn.com/dms/image/C5103AQFSMSUsGWxVHA/profile-displayphoto-shrink_800_800/0/1516866499495?e=1653523200&v=beta&t=aZDbrqMjjVEHXzIweV5zipz4yS01Lf0uYA3I_mCdjCU',
         email: 'danielmdenton@gmail.com',
         bio: 'Daniel is a software engineer with a background as a yoga and meditation teacher as well as a working musician. Whether a blank canvas or an almost completed project, on a team or individually, he can be counted on to bring whatever creative piece is needed to complete the puzzle.',
-        linkedin: 'https://www.linkedin.com/in/danielmdenton/'
+        linkedin: 'https://www.linkedin.com/in/danielmdenton/',
+        github: 'https://github.com/danieldenton'
     }
 ]
 
